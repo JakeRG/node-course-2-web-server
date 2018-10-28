07b18b427c3436070665cb1c17ef9e890a63162d
Jake Robin van der Ploeg <jake.r.g@gmail.com>
2018-10-28 20:26:15 +0100

    Added projects page

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -39,6 +39,12 @@ app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home page',
         welcomeMessage: 'This is a welcome message!',       
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
     });
 });
 
